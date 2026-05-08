2ca7bb4f02707707376b6c1880752e7a132e7d67
Valeriya Nikolkina <valeriyanikolkina@Valeriyas-MacBook-Air.local>
2026-05-08 11:49:13 +0200

    Finished CRUD and documentation

--- src/dao/mealDao.js
+++ src/dao/mealDao.js
@@ -1,25 +1,55 @@
-const fs = require('fs').promises;
-const path = require('path');
+const meals = require("../data/meals.json");
 
-const DATA_PATH = path.join(__dirname, '..', 'data', 'meals.json');
-
-async function readMeals() {
-  const raw = await fs.readFile(DATA_PATH, 'utf8');
-  return JSON.parse(raw || '[]');
-}
-
-async function writeMeals(meals) {
-  await fs.writeFile(DATA_PATH, JSON.stringify(meals, null, 2), 'utf8');
+function getAllMeals() {
+  return meals;
 }
 
 module.exports = {
-  getAll: async () => readMeals(),
-  add: async (meal) => {
-    const meals = await readMeals();
-    const nextId = meals.length ? Math.max(...meals.map(m => m.id)) + 1 : 1;
-    const newMeal = { id: nextId, ...meal };
-    meals.push(newMeal);
-    await writeMeals(meals);
-    return newMeal;
-  },
+  getAllMeals,
+};
+const meals = require("../data/meals.json");
+
+function getAllMeals() {
+  return meals;
+}
+
+module.exports = {
+  getAllMeals,
+};
+const meals = require("../data/meals.json");
+
+function getAllMeals() {
+  return meals;
+}
+
+module.exports = {
+  getAllMeals,
+};
+const meals = require("../data/meals.json");
+
+function getAllMeals() {
+  return meals;
+}
+
+module.exports = {
+  getAllMeals,
+};
+const meals = require("../data/meals.json");
+
+function getAllMeals() {
+  return meals;
+}
+
+module.exports = {
+  getAllMeals,
+};
+const fs = require('fs').promises;
+const meals = require("../data/meals.json");
+
+function getAllMeals() {
+  return meals;
+}
+
+module.exports = {
+  getAllMeals,
 };
